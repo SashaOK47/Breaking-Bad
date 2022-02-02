fbd4f648188c9a4cc1ad5983d84ae28d77ae5f40
Sasha Okoronko <hvorost22@mail.ru>
2022-02-02 18:09:54 +0300

    BB-9: - made a header

---
+++ src/components/organisms/Header/index.js
@@ -0,0 +1,29 @@
+import style from "./styles.module.scss";
+import { NavLink } from "react-router-dom";
+import logo from "../../../assets/images/logo.png";
+import burger from "../../../assets/icons/burger.svg";
+
+const Header = () => {
+  return (
+    <header className={style.header}>
+      <div className={style.container}>
+        <nav className={style.navigation}>
+          <NavLink to="/" className={style.logo}>
+            <img src={logo} alt="Logo" />
+          </NavLink>
+          <NavLink
+            end
+            to="/characters"
+            className={({ isActive }) => `${isActive ? style.active : ""}`}
+          >
+            <span className={style.catalog}>
+              <img src={burger} alt="Burger" />
+              <button>Каталог</button>
+            </span>
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+};
+export default Header;
