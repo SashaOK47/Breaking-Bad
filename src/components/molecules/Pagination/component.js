000b63d330a19f92274ca4b0e8c9a1f6f83f8e0f
Sasha Okoronko <hvorost22@mail.ru>
2022-02-07 10:25:02 +0300

    BB-14: - created the component structure Pagination

---
+++ src/components/molecules/Pagination/component.js
@@ -0,0 +1,11 @@
+// import PropTypes from "prop-types";
+import style from "./styles.module.scss";
+
+const Pagination = () => {
+  return (
+    <div>
+      <h2>Pagination</h2>
+    </div>
+  );
+};
+export default Pagination;
---
+++ src/components/molecules/Pagination/container.js
@@ -0,0 +1,6 @@
+import Pagination from "./component";
+
+const PaginationContainer = () => {
+  return <Pagination />;
+};
+export const container = PaginationContainer;
